092f844473374ae00f595379233a576d4d7cd153
Max <sobolmax1984@mail.com>
2019-12-18 00:09:00 +0200

    Pilot 2

---
+++ src/ua/com/pointer/Dog.java
@@ -0,0 +1,11 @@
+package ua.com.pointer;
+
+public class Dog {
+
+    String name = "NEW DOG";
+    String color = "RED";
+    public String Name = "NEW DOG - Tuzik";
+    public int age = 28;
+    public int salary = 2500;
+
+}
---
+++ src/ua/com/pointer/Person.java
@@ -0,0 +1,12 @@
+package ua.com.pointer;
+
+public class Person {
+
+    public String firstName = "Sasha";
+    public int age = 28;
+    public int salary = 1000;
+
+    public void say(){
+        System.out.println("Salary is 1000$");
+    }
+}
